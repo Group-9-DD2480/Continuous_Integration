5d9843c5684bfec3cab0f577c2c527d1d8e434de
jonhedin <jonhedin@kth.se>
2022-02-14 12:50:50 +0100

    feat: Added file for unit testing, issue #20

---
+++ src/test/java/CITest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CITest {
+
+}
